6a6054990d95218cf6921645d55b9179ffb51fff
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-06-30 16:32:11 +0800

    Fix eslint offences

--- src/App.js
+++ src/App.js
@@ -10,18 +10,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">
-          Welcome to React
-        </h1>
+        <h1 className="App-title">Welcome to React</h1>
       </header>
       <p className="App-intro">
-        To get started, edit
-        {' '}
-        <code>
-          src/App.js
-        </code>
-        {' '}
-        and save to reload.
+        To get started, edit <code>src/App.js</code> and save to reload.
       </p>
     </div>
   );
